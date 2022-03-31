eb5839a49c8fc8bb09fd02dd5122676ef354c337
Kirill Ki <kirillkalapov@gmail.com>
2022-04-01 00:51:59 +0200

    in progress

--- MVVM/MainModule/View/MainViewController.swift
+++ MVVM/MainModule/View/MainViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 class MainViewController: UIViewController {
     
     private var viewModel: MainViewModelProtocol!
+    private var testView: TestView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        ///????
+       viewModel = MainViewModel()
+        
+        createView()
+        updateView()
+        
     }
 
+    private func createView() {
+        testView = TestView()
+        testView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        testView.center = view.center
+        view.addSubview(testView)
+    }
 
+    private func updateView() {
+        viewModel.updateViewData = { [weak self] viewData in
+            self?.testView.viewData = viewData
+        }
+    }
+    
 }
 
---
+++ MVVM/ModuleBuilder.swift
@@ -0,0 +1,22 @@
+//
+//  ModuleBuilder.swift
+//  MVVM
+//
+//  Created by  Mr.Ki on 01.04.2022.
+//
+
+import UIKit
+//
+//protocol Builder {
+//    static func createMain() -> UIViewController
+//}
+//
+//class ModuleBuilder: Builder {
+//    static func createMain() -> UIViewController {
+//        let model = ViewData.Data(icon: "success", title: "Success", description: "Hello", numberPhone: "32227")
+//        let view = TestView()
+//        let vm = MainViewModel(
+//    }
+//    
+//    
+//}
